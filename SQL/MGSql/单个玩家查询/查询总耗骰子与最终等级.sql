983b69b886e55f0c8d4083e162bd264dd5d0cedf
dengzhilei <707304943@qq.com>
2026-01-28 20:53:29 +0800

    update

--- SQL/MGSql/单个玩家查询/查询总耗骰子与最终等级.sql
+++
@@ -1,7 +0,0 @@
-select sum(cast(proj_asset_value as bigint)) as cost_dice,max(cast(proj_level as int)) as final_level
-from table.event_20652
-where event_id='asset_circulate'
-and day ${PartDate:date1}
-and proj_asset_id = '100100'
-and proj_asset_change_type='2'
-and proj_simple_game_id='20021892'
